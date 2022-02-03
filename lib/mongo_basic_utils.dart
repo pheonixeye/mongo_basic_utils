120e2f4738f25e3fcfd4efd8c7362ea580b5385c
pheonixeye <65448447+pheonixeye@users.noreply.github.com>
2022-02-03 20:22:10 +0200

    2nd

--- lib/mongo_basic_utils.dart
+++ lib/mongo_basic_utils.dart
@@ -6,3 +6,14 @@ library mongo_basic_utils;
 export 'src/mongo_basic_utils_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
+export 'src/DnsApiProvider.dart';
+export 'src/DnsUtils.dart';
+export 'src/HttpUtils.dart';
+export 'src/RRecordType.dart';
+
+//models
+export 'model/HttpRequestReturnType.dart';
+export 'model/Question.dart';
+export 'model/RRecord.dart';
+export 'model/ResolveResponse.dart';
+export 'model/exception/HttpResponseException.dart';
